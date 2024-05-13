465606b5e165a2039475d90832bca44e9a735150
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-05-13 13:01:08 +0200

    update code

--- 05460A_Galliano_Andrea.go
+++ 05460A_Galliano_Andrea.go
@@ -1,20 +1,62 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 type piastrella struct {
+	x      int
+	y      int
+	colore string
+	accesa bool
 }
 
-type regola struct {
+type addendoRegola struct {
+	coefficiente int
+	colore       string
 }
 
 type piano struct {
 	piastrelle []piastrella
-	regole     []regola
+	regole     []addendoRegola
 }
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	parseInput(scanner)
+
 	fmt.Println("ciao mondo")
+}
+
+func parseInput(scanner *bufio.Scanner) {
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		if input == "q" {
+			break
+		}
+
+		if len(input) > 0 {
+			command := input[:1]
+			// argument := strings.TrimSpace(input[1:])
+
+			switch command {
+			case "C":
+				fmt.Println("colora")
+			case "S":
+				fmt.Println("spegni")
+			case "r":
+				fmt.Println("regola")
+			case "?":
+				fmt.Println("stato")
+			case "s":
+				fmt.Println("stampa")
+			default:
+				fmt.Println("comando non valido")
+			}
+		}
+	}
 }
