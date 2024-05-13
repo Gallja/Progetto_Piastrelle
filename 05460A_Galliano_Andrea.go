e3de3b6c044fdc58019263c17046fcca6c579911
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2024-05-13 11:42:04 +0200

    update file's name

---
+++ 05460A_Galliano_Andrea.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type piastrella struct {
+}
+
+type regola struct {
+}
+
+type piano struct {
+	piastrelle []piastrella
+	regole     []regola
+}
+
+func main() {
+	fmt.Println("ciao mondo")
+}
